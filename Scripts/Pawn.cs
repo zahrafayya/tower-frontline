3f8b09cd597f63b05a52b8c36d7233ce5b1f6160
Zahra Fayyadiyati <zahra.fayyadiyati@gmail.com>
2023-05-28 14:45:06 +0700

    feat: walking

--- Scripts/Pawn.cs
+++ Scripts/Pawn.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,31 +6,88 @@ using UnityEngine;
 public class Pawn : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private bool isRunning = true;
+    [SerializeField] private int idleDuration;
+    
+    private GameObject childObjectAnimation;
+    private Animator animator;
+
+    private GameObject childObjectHealth;
+    
+    private String currentAction = "";
+    private String promptedAction = "Run";
+    public enum Action
+    {
+        Run,
+        Attack,
+        Idle
+    }
+
+    public void GetAction()
+    {
+        
+    }
+
+    // Animation Controller
+    private void SetAnimationRun()
+    {
+        animator.SetBool("isAttack", false);
+        animator.SetBool("isRun", true);
+    }
+    
+    private void SetAnimationAttack()
+    {
+        animator.SetBool("isAttack", true);
+        animator.SetBool("isRun", false);
+    }
+    
+    // Action Controller
     private void Run()
     {
+        if (!currentAction.Equals("Run"))
+        {
+            SetAnimationRun();
+            currentAction = "Run";
+        }
+        
+
         float moveAmount = speed * Time.deltaTime;
 
         transform.Translate(Vector3.right * moveAmount);
 
     }
 
+    private void Attack()
+    {
+        if (!currentAction.Equals("Attack"))
+        {
+            SetAnimationAttack();
+            currentAction = "Attack";
+        }
+    }
+
     void Start()
     {
-          
+        childObjectAnimation = transform.Find("Animation").gameObject;
+        animator = childObjectAnimation.GetComponent<Animator>();
+        
+        childObjectHealth = transform.Find("HealthBar").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isRunning) Run(); 
+        if(promptedAction.Equals("Run")) Run(); 
+        else if (promptedAction.Equals("Attack"))
+        {
+            Attack();
+        }
     }
     
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        // if (collision.gameObject.CompareTag("Target"))
-        // {
-        isRunning = false;
-        // }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            promptedAction = "Attack";
+        }
     }
 }
